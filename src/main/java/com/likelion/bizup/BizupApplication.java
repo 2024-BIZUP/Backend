1208cdd97822f6cd484b75e7c98748d040a99a08
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-10-04 22:11:26 +0900

    feat: 프로젝트 기본 세팅 (#2)

---
+++ src/main/java/com/likelion/bizup/BizupApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.bizup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizupApplication.class, args);
+	}
+
+}
